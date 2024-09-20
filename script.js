a7ba60e2760260fbfbc630fd8df0b347595d9f93
Sujata255 <das19sujata@gmail.com>
2024-09-20 19:26:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,6 @@
-//your JS code here. If required.
+function capitalizeName() {
+  const inputField = document.getElementById("fname");
+  inputField.value = inputField.value.toUpperCase();
+}
+
+document.getElementById("fname").addEventListener("blur", capitalizeName);
